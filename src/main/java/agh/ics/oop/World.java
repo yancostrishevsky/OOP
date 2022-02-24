a3e67f412d4c02c98f8db9331a89d58077d39d76
yancostrishevsky <88140201+yancostrishevsky@users.noreply.github.com>
2022-02-24 12:06:49 +0100

    Update World.java

--- src/main/java/agh/ics/oop/World.java
+++ src/main/java/agh/ics/oop/World.java
@@ -2,22 +2,7 @@ package agh.ics.oop;
 
 public class World {
     public static void main(String[] args) {
-        //System.out.println("SYSTEM WYSTARTOWAL");
-        //
-        //run(change(argumenty));
-        //System.out.println();
-        //System.out.println("SYSTEM ZAKONCZYL DZIALANIE");
-        //Vector2d position1 = new Vector2d(1,2);
-        //System.out.println(position1);
-        //Vector2d position2 = new Vector2d(-2,1);
-        //System.out.println(position2);
-        //System.out.println(position1.add(position2));
-        Animal zwierz = new Animal();
-        String[] argumenty = {"r","l","b","f"};
-        MoveDirection[] directions = OptionsParser.parse(argumenty);
-        for (MoveDirection dir:directions) {
-            zwierz.move(dir);
-            System.out.println(zwierz);
+       
 
         }
     }
@@ -38,11 +23,11 @@ public class World {
     public static void run(Direction[] args){
         for(Direction text: args){
             String message = switch(text) {
-                case FORWARD -> "Do przodu ";
-                case BACKWARD -> "Do tyłu ";
-                case RIGHT -> "W prawo ";
-                case LEFT -> "W lewo ";
-                default -> "Nieznana komenda ";
+                case FORWARD -> "forward ";
+                case BACKWARD -> "bacward ";
+                case RIGHT -> "right ";
+                case LEFT -> "left ";
+                default -> "unknown command ";
             };
             System.out.print(message);
         }
